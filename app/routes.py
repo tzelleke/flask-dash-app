58b1c0682c9634fa2670b095d9c7752034ffd339
tzelleke <theodros.zelleke@gmail.com>
2023-03-12 19:32:15 +0100

    feat: updates theme

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,7 @@ from markupsafe import Markup
 def index():
     with open("README.md", "r") as fp:
         formatter = HtmlFormatter(
-            style="solarized-dark", full=True, cssclass="codehilite",
+            style="solarized-light", full=True, cssclass="codehilite",
         )
         styles = f"<style>{formatter.get_style_defs()}</style>"
         html = (
@@ -20,7 +20,7 @@ def index():
                 'src="app/',
                 'src="',
             )
-            .replace("codehilite", "codehilite p-2 mb-3 bg-dark")
+            .replace("codehilite", "codehilite p-2 mb-3")
         )
         return render_template(
             "index.html", content=Markup(html), styles=Markup(styles),
